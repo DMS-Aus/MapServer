ff7645cd51002e896ec74d64aac489373ec53bb0
Max Kellermann <max.kellermann@gmail.com>
2021-10-05 12:09:19 +0200

    mapshape: check msSHPReadBounds() return value, fix endless loop
    With a crafted shapefile, it was possible to put
    msShapefileWhichShapes() into an extremely long loop, calling
    msSHPReadBounds() over and over, even if all of those calls fail.
    
    This patch adds error checking, and if an error occurs,
    msShapefileWhichShapes() gives up, because after an I/O error, there
    is no reasonable chance that anything will ever work properly.
    
    Vulnerability found by libFuzzer.

--- mapshape.c
+++ mapshape.c
@@ -1747,7 +1747,10 @@ int msShapefileOpen(shapefileObj *shpfile, const char *mode, const char *filenam
     return -1;
   }
 
-  msSHPReadBounds( shpfile->hSHP, -1, &(shpfile->bounds));
+  if( msSHPReadBounds( shpfile->hSHP, -1, &(shpfile->bounds)) != MS_SUCCESS ) {
+    msSHPClose(shpfile->hSHP);
+    return -1;
+  }
 
   bufferSize = strlen(filename)+5;
   dbfFilename = (char *)msSmallMalloc(bufferSize);
@@ -1876,8 +1879,10 @@ int msShapefileWhichShapes(shapefileObj *shpfile, rectObj rect, int debug)
       }
 
       for(i=0; i<shpfile->numshapes; i++) {
-        if(msSHPReadBounds(shpfile->hSHP, i, &shaperect) == MS_SUCCESS)
-          if(msRectOverlap(&shaperect, &rect) == MS_TRUE) msSetBit(shpfile->status, i, 1);
+        if(msSHPReadBounds(shpfile->hSHP, i, &shaperect) != MS_SUCCESS)
+          return(MS_FAILURE);
+
+        if(msRectOverlap(&shaperect, &rect) == MS_TRUE) msSetBit(shpfile->status, i, 1);
       }
     }
   }
